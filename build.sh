95e612ededea2d462653d2bc1952c22abbb7b934
David <duwagbale07@gmail.com>
2025-04-03 22:12:54 +0100

    Fix (build)

--- build.sh
+++ build.sh
@@ -6,7 +6,4 @@ pip install -r requirements.txt
 echo "Running migrations..."
 python manage.py migrate
 
-echo "Collecting static files..."
-python manage.py collectstatic --no-input
-
 echo "Starting deployment..."
