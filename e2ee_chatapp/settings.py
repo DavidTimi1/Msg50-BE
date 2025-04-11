a23006971882a41b02176734f670aa13dd885df2
David <duwagbale07@gmail.com>
2025-04-11 04:46:10 +0100

    Config (postgres-migration)

--- e2ee_chatapp/settings.py
+++ e2ee_chatapp/settings.py
@@ -101,11 +101,20 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+USE_SQLITE = os.environ.get("USE_SQLITE", "False") == "True"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} if DEBUG and USE_SQLITE else {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
